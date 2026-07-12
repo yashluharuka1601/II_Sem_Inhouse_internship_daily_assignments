7e698a807b2a8f61171ce4e48c09ad1e1d2cc440
yashluharuka1601 <yashluharuka1601@gmail.com>
2026-07-12 23:22:46 +0530

    Create access_denied.php

---
+++ 25ESKCS373/DAY11/access_denied.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+
+<div style="text-align:center;margin-top:100px">
+
+<h1>
+403 Access Denied
+</h1>
+
+<p>
+You are not allowed to access this page.
+</p>
+
+<a href="login.php">
+Go Login
+</a>
+
+</div>
+
+
+</body>
+
+</html>
